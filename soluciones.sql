bfda2f785f62c5059d69a240e44d4013a1216805
tgarbarino <garbarinothiago@gmail.com>
2026-08-09 22:44:26 -0300

    Add files via upload

---
+++ soluciones.sql
@@ -0,0 +1,29 @@
+-- ══════════════════════════════════════════
+-- MiniStore — Soluciones con Outer JOINs
+-- Autor: [THIAGO GARBARINO]
+-- Fecha: [08/08/2026]
+-- ══════════════════════════════════════════
+
+-- ── CONSULTA 1: LEFT JOIN ─────────────────
+-- Pregunta de negocio: ¿Qué productos del catálogo nunca fueron vendidos?
+-- Mostrá todos los productos y sus ventas asociadas.
+-- Los productos sin ventas aparecerán con NULL en las columnas de ventas.
+
+-- [Tu consulta aquí]
+SELECT * FROM PRODUCTOS LEFT JOIN VENTAS ON PRODUCTOS.PRODUCTO_ID = VENTAS.PRODUCTO_ID
+
+-- ── CONSULTA 2: RIGHT JOIN ────────────────
+-- Pregunta de negocio: ¿Existen ventas registradas con productos
+-- que no figuran en nuestro catálogo? (posible error de carga de datos)
+-- Los registros huérfanos aparecerán con NULL en las columnas de productos.
+
+-- [Tu consulta aquí]
+SELECT * FROM PRODUCTOS RIGHT JOIN VENTAS ON PRODUCTOS.PRODUCTO_ID = VENTAS.PRODUCTO_ID
+
+-- ── CONSULTA 3: FULL OUTER JOIN ───────────
+-- Pregunta de negocio: Vista completa de auditoría que muestre
+-- todos los productos y todas las ventas sin perder ninguna fila,
+-- identificando tanto productos sin ventas como ventas sin producto.
+
+-- [Tu consulta aquí]
+SELECT * FROM PRODUCTOS FULL OUTER JOIN VENTAS ON PRODUCTOS.PRODUCTO_ID = VENTAS.PRODUCTO_ID
